17730e4c5c92aebc834fceec0218a2ee6cfd879e
Manuel Rivero <manuel.enrique.r.v@gmail.com>
2023-09-10 16:27:17 -0300

    [fix] authApi store import

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from '@reduxjs/toolkit/query'
 import authSlice from "./authSlice";
+import { authApi } from "../services/authApi";
 
 export const store = configureStore({
   reducer: {
